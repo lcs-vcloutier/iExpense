60fdd63954e3dc73ebfd35c166b211ffb7de1554
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-08 10:16:53 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Cloutier, Vincent on 2020-10-08.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
